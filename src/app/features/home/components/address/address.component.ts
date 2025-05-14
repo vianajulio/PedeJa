6e5796f4c2d62c8f824ccb0474a3728924b59f49
Julio Viana <105553703+vianajulio@users.noreply.github.com>
2025-05-14 18:59:47 -0300

    Ajuste no nome

--- src/app/features/home/components/address/address.component.ts
+++ src/app/features/home/components/address/address.component.ts
@@ -1,16 +1,42 @@
 import { Component, Input } from '@angular/core';
 import { LocalStorageService } from '../../../../services/local-storage.service';
+import { FormsModule } from '@angular/forms';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-address',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './address.component.html',
   styleUrl: './address.component.scss',
 })
 export class AddressComponent {
-  @Input() address?: string;
-  
+  @Input() address: string = '';
+
+  toastMessage: string = '';
+
   constructor(private localStorage: LocalStorageService) {
     this.address = localStorage.getAddress();
+  }
+
+  Save() {
+    if (!this.address.trim()) {
+      this.showToast("Preencha o endereço!");
+      return;
+    }
+    if (this.address == this.localStorage.getAddress()) {
+      this.showToast("Preencha com outro endereço!");
+      return;
+    }
+
+    this.localStorage.setAddress(this.address);
+  }
+
+  showToast(message: string) {
+    this.toastMessage = message;
+    const toastEl = document.getElementById('addressToast');
+    const toast = new bootstrap.Toast(toastEl);
+    toast.show();
+    return;
   }
 }
